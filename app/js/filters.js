a219efee9b2bf59ec237357cdc0de3e7d496542a
Eric Zou <ericzou@eqlink.com>
2012-12-31 13:31:20 -0800

    styled and formated tweets

--- app/js/filters.js
+++ app/js/filters.js
@@ -4,6 +4,16 @@
 angular.module('angular.dashboard.filters', []).
   filter('autoLinkLite', function() {
     return function(text) {
-      return String(text).replace(/(https?:\/\/[^\s]*)/, "<a href=\"$1\">$1</a>");
+      return String(text).replace(/(https?:\/\/[^\s]*)/g, "<a href=\"$1\">$1</a>");
+    }
+  }).
+  filter('parseTwitterUserTag', function() {
+    return function(text) {
+      return String(text).replace(/\s@(.+)\b/g, "<a href=\"https://www.twitter.com/$1\"> @$1</a>")
+    }
+  }).
+  filter('epochTime', function() {
+    return function(text) {
+      return new Date(text).getTime();
     }
   });
